7c3a7c6ba64d7be9095a162a10412e8677e44dfe
TobiasMaugus <tobiasmaugus705@gmail.com>
2022-08-31 09:45:27 -0300

    l

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
-export { default as LoadingComp } from "./Loading"
+export {default as ButtonComp} from "./Button" 
+export {default as CardSocialComp} from "./CardSocial" 
+export {default as CardComp} from "./Card" 
+export {default as LoadingComp} from "./Loading" 
